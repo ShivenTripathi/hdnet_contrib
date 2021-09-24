5f6bc403e3b44f4c44f71b7029e79dcd3d14ed75
Shiven <shiven@iitk.ac.in>
2021-09-25 01:01:40 +0530

    update demos for without source parameter

--- hdnet_contrib/PyCDMentropy/CDMentropy/try_me.m
+++ hdnet_contrib/PyCDMentropy/CDMentropy/try_me.m
@@ -1,3 +1,4 @@
+
 % This is a toy script to guide you to using CDMentropy
 % with a toy binary word observations.
 
@@ -64,17 +65,17 @@ fprintf('CDM entropy estimate [%f bits] under Bernoulli prior\n', H3);
 
 %% Visualizing the word count histogram
 % Convert the words to unique identities
-% symSeq = discreteTimeSeries2Words(uint16(words'), 2);
-% [xrange, uidx] = unique(symSeq); % Unique words
-% counts = histc(symSeq, xrange);
-% wordstrs = reshape(sprintf('%d', words(uidx,:))', [], m);
+symSeq = discreteTimeSeries2Words(uint16(words'), 2);
+[xrange, uidx] = unique(symSeq); % Unique words
+counts = histc(symSeq, xrange);
+wordstrs = reshape(sprintf('%d', words(uidx,:))', [], m);
 
-% figure(8490);
-% bh = bar(xrange, counts);
-% set(bh, 'FaceColor', 'none');
-% set(gca, 'XTickLabel', wordstrs, 'YTick', 0:max(counts)+1);
-% title('Binary word histogram');
-% ylim([0 max(counts)+1]); ylabel('# of observations');
+figure(8490);
+bh = bar(xrange, counts);
+set(bh, 'FaceColor', 'none');
+set(gca, 'XTickLabel', wordstrs, 'YTick', 0:max(counts)+1);
+title('Binary word histogram');
+ylim([0 max(counts)+1]); ylabel('# of observations');
 
 % If you need the variance 
 opts.nMC = 999; % control the # of samples from posterior
@@ -82,21 +83,21 @@ opts.verbose = true;
 [H4, Hvar] = computeH_CDM(nn, ocnts, m, opts);
 fprintf('CDM entropy estimate [%f +/- %f bits]\n', H4, sqrt(Hvar));
 
-% % Generating the multiplicities form (perhaps for using other estimators)
-% counts = fastWords2Counts(uint16(words'), 2); % equivalent to above counts
-% [mm, icts] = multiplicitiesFromCounts(counts);
-% % [mm, icts] = words2multiplicities(words, 2); % equivalently
-% H5 = computeH_PYM(mm, icts); % estimate entropy using PYM for fun
+% Generating the multiplicities form (perhaps for using other estimators)
+counts = fastWords2Counts(uint16(words'), 2); % equivalent to above counts
+[mm, icts] = multiplicitiesFromCounts(counts);
+% [mm, icts] = words2multiplicities(words, 2); % equivalently
+H5 = computeH_PYM(mm, icts); % estimate entropy using PYM for fun
 
 % If you need the samples from the posterior
 opts.verbose = false;
 [H6, Hvar, CIhandle, ~, Hsamples, optsOut] = computeH_CDM(nn, ocnts, m, opts);
 figure(1470); cla; hold on;
-%ksdensity(Hsamples);
+ksdensity(Hsamples);
 ph = zeros(3, 1);
 ph(1) = plot(H1 * [1 1], [0 0.1], 'o-');
 ph(2) = plot(H2 * [1 1], [0 0.2], 'x-');
-%ph(3) = plot(H5 * [1 1], [0 0.3], '*-');
+ph(3) = plot(H5 * [1 1], [0 0.3], '*-');
 xlabel('entropy (bits)'); ylabel('distribution');
 legend(ph, 'CDM (Syn)', 'CDM (Ber)', 'PYM');
 
